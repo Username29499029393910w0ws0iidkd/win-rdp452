7c51d1edb80f1c00406a5b1e41135cd96958a753
Username29499029393910w0ws0iidkd <n94025921@gmail.com>
2025-06-05 13:41:28 +0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,28 @@
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-exit
+@echo off
+:: Проверка прав администратора
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [!] Пожалуйста, запустите этот скрипт от имени администратора.
+    pause
+    exit /b
+)
+
+echo [+] Отключение фаервола...
+netsh advfirewall set allprofiles state off
+
+echo [+] Загрузка и установка Chrome Remote Desktop Host...
+set "crd_installer=%TEMP%\chromeremotedesktophost.msi"
+powershell -Command "Invoke-WebRequest -Uri 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile '%crd_installer%'"
+msiexec /i "%crd_installer%" /quiet /norestart
+del "%crd_installer%"
+
+echo [+] Загрузка и установка Google Chrome...
+set "chrome_installer=%TEMP%\chrome_installer.exe"
+powershell -Command "Invoke-WebRequest -Uri 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile '%chrome_installer%'"
+"%chrome_installer%" /silent /install
+del "%chrome_installer%"
+
+echo [+] Готово.
+pause
+exit /b
+
